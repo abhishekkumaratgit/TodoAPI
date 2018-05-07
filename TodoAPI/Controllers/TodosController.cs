5831c638041f68219a5f590bd69a36b915373512
abhishekkumaratgit <abhishekkumaratgit@gmail.com>
2018-05-07 18:26:38 +0530

    added Todo controller , deleted values controller

---
+++ TodoAPI/Controllers/TodosController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoAPI.Models;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodosController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodosController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.Todos.Count() == 0)
+            {
+                _context.Todos.Add(new Todo { Name = "Buy milk", Id = 1, IsComplete = false });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Todos
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            return _context.Todos;
+        }
+
+        // GET: api/Todos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var todo = await _context.Todos.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        // PUT: api/Todos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Todos
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Todos.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Get", new { id = todo.Id }, todo);
+        }
+
+        // DELETE: api/Todos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.Todos.SingleOrDefaultAsync(m => m.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _context.Todos.Any(e => e.Id == id);
+        }
+    }
+}
