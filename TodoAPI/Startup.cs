49bcccba5e071ca3b71a63e9e0bf5aa88bac7582
abhishekkumaratgit <abhishekkumaratgit@gmail.com>
2018-05-07 18:31:28 +0530

    Added remaining changes

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using TodoAPI.Models;
 
 namespace TodoAPI
 {
@@ -16,7 +14,7 @@ namespace TodoAPI
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            //Todo 1 
+            //Todo 1
         }
 
         public IConfiguration Configuration { get; }
@@ -24,7 +22,15 @@ namespace TodoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddDbContext<TodoContext>(opt =>
+                                opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc()
+                .AddXmlDataContractSerializerFormatters();
+
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +40,28 @@ namespace TodoAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //to allow to send static files like js, css and images
+            //app.UseStaticFiles();
+
+            //configue static file with chaching
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=600");
+                }
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                // c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
